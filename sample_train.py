8ac4c5508d06d9fae601d3c8b055b885e739db33
asahi417 <spacefunkspacefunk@gmail.com>
2017-11-25 17:12:24 +0900

    refactor

--- sample_train.py
+++ sample_train.py
@@ -8,48 +8,46 @@ import numpy as np
 def get_options(parser):
     parser.add_argument('model', action='store', nargs='?', const=None, default='char_cnn', type=str, choices=None,
                         metavar=None, help='Name of model to use. (default: char_cnn)')
-    parser.add_argument('-e', '--epoch', action='store', nargs='?', const=None, default=150, type=int,
-                        choices=None, help='Epoch number. (default: 150)', metavar=None)
-    parser.add_argument('-b', '--batch', action='store', nargs='?', const=None, default=500, type=int,
-                        choices=None, help='Batch size. (default: 500)', metavar=None)
-    parser.add_argument('-l', '--lr', action='store', nargs='?', const=None, default=0.001, type=float,
-                        choices=None, help='Learning rate. (default: 0.001)', metavar=None)
+    parser.add_argument('-e', '--epoch', action='store', nargs='?', const=None, default=300, type=int,
+                        choices=None, help='Epoch number. (default: 300)', metavar=None)
+    parser.add_argument('-b', '--batch', action='store', nargs='?', const=None, default=100, type=int,
+                        choices=None, help='Batch size. (default: 100)', metavar=None)
+    parser.add_argument('-l', '--lr', action='store', nargs='?', const=None, default=0.0001, type=float,
+                        choices=None, help='Learning rate. (default: 0.0001)', metavar=None)
     parser.add_argument('-c', '--clip', action='store', nargs='?', const=None, default=None, type=float,
                         choices=None, help='Gradient clipping. (default: None)', metavar=None)
     parser.add_argument('-k', '--keep', action='store', nargs='?', const=None, default=1, type=float,
                         choices=None, help='Keep rate for Dropout. (default: 1)', metavar=None)
-    parser.add_argument('-v', '--valid', action='store', nargs='?', const=None, default=2, type=int,
-                        choices=None, help='Validation chunk size. (default: 2)', metavar=None)
     return parser.parse_args()
 
 
 def get_model_instance(model_name, label_size=2):
     # set up pre processing
     w2v = gensim.models.KeyedVectors.load_word2vec_format("./data/GoogleNews-vectors-negative300.bin", binary=True)
+    _pre_process = sequence_modeling.Process("embed", {"length_word": 40, "dim": w2v.vector_size, "model": w2v})
+
+    def _model_inputs(model, x): return {model.x: np.expand_dims(x, 3)}
 
     if model_name == "char_cnn":
         _net = {"label_size": label_size, "input_char": [40, 33, 26], "input_word": [40, 300],
                 "char_embed_dim": 5, "char_cnn_unit": 10, "char_cnn_kernel": 3, "word_embed_dim": 30,
                 "cnn_unit": 300, "cnn_kernel": 5, "hidden_unit": 300}
         __model = sequence_modeling.model.CharCNN
-        _pre_process = [sequence_modeling.Process("onehot", {"length_word": 40, "length_char": 33}),
-                        sequence_modeling.Process("embed", {"length_word": 40, "dim": w2v.vector_size, "model": w2v})]
+        _pre_process = [sequence_modeling.Process("onehot", {"length_word": 40, "length_char": 33}), _pre_process]
 
-        def _model_inputs(model, x):
-            return {model.x_char: x[0], model.x_word: x[1]}
-
+        def _model_inputs(model, x): return {model.x_char: x[0], model.x_word: x[1]}
     elif model_name == "gap_cnn":
         _net = {"label_size": label_size, "n_input": [40, 300, 1]}
         __model = sequence_modeling.model.GapCNN
-        _pre_process = sequence_modeling.Process("embed", {"length_word": 40, "dim": w2v.vector_size, "model": w2v})
-
-        def _model_inputs(model, x):
-            return {model.x: np.expand_dims(x, 3)}
-
+    elif model_name == "deep_cnn":
+        _net = {"label_size": label_size, "n_input": [40, 300, 1]}
+        __model = sequence_modeling.model.DeepCNN
+    elif model_name == "shallow_cnn":
+        _net = {"label_size": label_size, "n_input": [40, 300, 1]}
+        __model = sequence_modeling.model.ShallowCNN
     elif model_name == "lstm":
         _net = {"label_size": label_size, "n_input": [40, 300], "n_hidden_1": 64, "n_hidden_2": 128, "n_hidden_3": 256}
         __model = sequence_modeling.model.LSTM
-        _pre_process = sequence_modeling.Process("embed", {"length_word": 40, "dim": w2v.vector_size, "model": w2v})
         _model_inputs = None
     else:
         raise ValueError("unknown model!")
